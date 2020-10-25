8141fc5ceed5993462a09d5964da2324f627f0ad
Ravi Jaiswal <ravikj17@gmail.com>
2020-10-25 20:41:05 +0530

    Permutation_Of_Words

--- DSA/src/Graph/Permutation_Of_Words.java
+++ DSA/src/Graph/Permutation_Of_Words.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Permutaion_Of_Words {
+public class Permutation_Of_Words {
 
 	public static void main(String[] args) throws IOException {
 
