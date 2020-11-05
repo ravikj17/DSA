4ad7e5b0c8147a525a09d41d57e4d3d49eb7a6a2
Ravi Jaiswal <ravikj17@gmail.com>
2020-11-05 18:14:33 +0530

    Linked_List_Cycle

---
+++ DSA/src/linkedlist/singlylinkedlist/Linked_List_Cycle.java
@@ -0,0 +1,43 @@
+package linkedlist.singlylinkedlist;
+
+/*
+ * Leetcode 141. Linked List Cycle
+Easy
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
+
+public class Linked_List_Cycle {
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null)
+			return false;
+		if (head == null)
+			return true;
+		if (head.next == head)
+			return true;
+		ListNode slow = head, fast = head.next;
+		while (slow != null && fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
